675ff9e8e2baa05c61472d4d2a01865045dacf61
JorderGomes <jordernoveau@gmail.com>
2024-05-16 16:11:47 -0300

    updating project to module.ts

--- estudo-angular/src/app/app.component.spec.ts
+++ estudo-angular/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '01-helloWorld' title`, () => {
+  it(`should have as title 'estudo-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('01-helloWorld');
+    expect(app.title).toEqual('estudo-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 01-helloWorld');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, estudo-angular');
   });
 });
