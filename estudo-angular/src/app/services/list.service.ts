8000e6296d626512d6381a9314ad879a9e5c73b1
JorderGomes <jordernoveau@gmail.com>
2024-05-18 11:28:38 -0300

    http requests

--- estudo-angular/src/app/services/list.service.ts
+++ estudo-angular/src/app/services/list.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Register } from '../Register';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
 
-  constructor() { }
+  private apiUrl = "http://localhost:3000/registers";
+
+  constructor(private http: HttpClient) { }
 
   remove(registers: Register[], register: Register){
     return registers.filter((r) => register.id !== r.id);
+  }
+
+  getAll(): Observable<Register[]>{
+    return this.http.get<Register[]>(this.apiUrl);
   }
 
 }
