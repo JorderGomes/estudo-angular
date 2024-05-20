455d695e65ea556576876dc95ae62be5f2e7fbad
JorderGomes <jordernoveau@gmail.com>
2024-05-20 11:30:37 -0300

    get data from api

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MomentService } from '../../../services/moment.service';
+import { Moment } from '../../../moment';
+import { enviroment } from '../../../../enviroments/enviroment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = enviroment.baseApiUrl;
+
+  //todo: search
+
+  constructor(
+    private momentService: MomentService
+  ){}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleString('pt-BR');
+      });
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
+
 
 }
--- moments/src/app/services/moment.service.ts
+++ moments/src/app/services/moment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Moment } from '../moment';
+import { Response } from '../Response';
 import { enviroment } from '../../enviroments/enviroment';
 
 @Injectable({
@@ -15,6 +16,10 @@ export class MomentService {
 
   createMoment(formData: FormData): Observable<FormData>{
     return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
+  getMoments(): Observable<Response<Moment[]>>{
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
   }
 
 }
