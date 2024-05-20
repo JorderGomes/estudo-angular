e479c48a11f82c6eebe833961c8d79ed916f5f05
JorderGomes <jordernoveau@gmail.com>
2024-05-20 17:03:10 -0300

    delete moment

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -4,6 +4,7 @@ import { Moment } from '../../../moment';
 import { Router, ActivatedRoute } from '@angular/router';
 import { enviroment } from '../../../../enviroments/enviroment';
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { MessagesService } from '../../../services/messages.service';
 
 @Component({
   selector: 'app-moment',
@@ -19,12 +20,20 @@ export class MomentComponent {
   
   constructor (
     private momentService: MomentService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     const id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
     this.momentService.getMoment(id).subscribe(item => this.moment = item.data);
+  }
+
+  async removeHandler(id:number){
+    await this.momentService.removeMoment(id).subscribe();
+    this.messagesService.addMessage("Momento excluído com sucesso!");
+    this.router.navigate(["/"]);
   }
 
 }
