260d8e58807f0b0c484efd20ce8412029983064f
JorderGomes <jordernoveau@gmail.com>
2024-05-20 14:54:27 -0300

    add search

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -14,23 +14,46 @@ export class HomeComponent {
   allMoments: Moment[] = [];
   moments: Moment[] = [];
   baseApiUrl = enviroment.baseApiUrl;
+  faSearch = faSearch;
+  searchTerm:string = '';
 
   //todo: search
 
   constructor(
     private momentService: MomentService
-  ){}
+  ) { }
 
   ngOnInit(): void {
     this.momentService.getMoments().subscribe((items) => {
       const data = items.data;
-      data.map((item) => {
-        item.created_at = new Date(item.created_at!).toLocaleString('pt-BR');
-      });
-      this.allMoments = data;
+
+      for (let i = 0; i < data.length; i++) {
+        data[i].created_at! = this.formatDate(data[i].created_at!);
+      }
+
+      this.allMoments = items.data;
       this.moments = data;
     });
   }
 
+  formatDate(date: string) {
+    const americanFormatRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}$/;
+    // const brazilianFormatRegex = /^\d{2}\/\d{2}\/\d{4}, \d{2}:\d{2}:\d{2}$/;
+    if (americanFormatRegex.test(date)) {
+      return new Date(date).toLocaleString('pt-BR')
+    } else  {
+      const [datePart, timePart] = date.split(', ');
+      return datePart;
+    }
+  }
+
+  search(e: Event): void{
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter(moment => {
+      return moment.title.toLocaleLowerCase().includes(value);
+    });
+  }
 
 }
