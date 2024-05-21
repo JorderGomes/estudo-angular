beb39d84ef2f24cc0980cd57a2553b415b2e2b51
JorderGomes <jordernoveau@gmail.com>
2024-05-21 11:48:17 -0300

    edit moment

--- moments/src/app/components/pages/edit-moment/edit-moment.component.ts
+++ moments/src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Moment } from '../../../moment';
 import { MomentService } from '../../../services/moment.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MessagesService } from '../../../services/messages.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -15,7 +16,9 @@ export class EditMomentComponent {
 
   constructor (
     private momentService: MomentService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router,
   ){}
 
   ngOnInit(): void {
@@ -23,6 +26,25 @@ export class EditMomentComponent {
     this.momentService.getMoment(id).subscribe(item => {
       this.moment = item.data;
     });
+  }
+
+  async editHandler(momentData: Moment){
+    const id = this.moment.id;
+    const formData = new FormData();
+
+    formData.append('title', momentData.title);
+    formData.append('description', momentData.description);
+
+    if (momentData.image){
+      formData.append('image', momentData.image);
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe();
+
+    this.messagesService.addMessage("Momento atualizado com sucesso!");
+
+    this.router.navigate(["/"]);
+
   }
 
 }
